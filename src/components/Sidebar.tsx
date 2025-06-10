a4f08469d041bae62f4d1567ea0942f92b327483
Yunus Emre Koyun <yunusemrekoyun26@gmail.com>
2025-06-10 15:25:22 +0300

    First Commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,133 @@
+"use client";
+import { navlinks } from "@/constants/navlinks";
+import { Navlink } from "@/types/navlink";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { Heading } from "./Heading";
+import { socials } from "@/constants/socials";
+import { Badge } from "./Badge";
+import { AnimatePresence, motion } from "framer-motion";
+import { IconLayoutSidebarRightCollapse } from "@tabler/icons-react";
+import { isMobile } from "@/lib/utils";
+
+export const Sidebar = () => {
+  const [open, setOpen] = useState(isMobile() ? false : true);
+
+  return (
+    <>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ x: -200 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.2, ease: "linear" }}
+            exit={{ x: -200 }}
+            className="px-6  z-[100] py-10 bg-neutral-100 max-w-[14rem] lg:w-fit  fixed lg:relative  h-screen left-0 flex flex-col justify-between"
+          >
+            <div className="flex-1 overflow-auto">
+              <SidebarHeader />
+              <Navigation setOpen={setOpen} />
+            </div>
+            <div onClick={() => isMobile() && setOpen(false)}>
+              <Badge href="/resume" text="Read Resume" />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <button
+        className="fixed lg:hidden bottom-4 right-4 h-8 w-8 border border-neutral-200 rounded-full backdrop-blur-sm flex items-center justify-center z-50"
+        onClick={() => setOpen(!open)}
+      >
+        <IconLayoutSidebarRightCollapse className="h-4 w-4 text-secondary" />
+      </button>
+    </>
+  );
+};
+
+export const Navigation = ({
+  setOpen,
+}: {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const pathname = usePathname();
+
+  const isActive = (href: string) => pathname === href;
+
+  return (
+    <div className="flex flex-col space-y-1 my-10 relative z-[100]">
+      {navlinks.map((link: Navlink) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          onClick={() => isMobile() && setOpen(false)}
+          className={twMerge(
+            "text-secondary hover:text-primary transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm",
+            isActive(link.href) && "bg-white shadow-lg text-primary"
+          )}
+        >
+          <link.icon
+            className={twMerge(
+              "h-4 w-4 flex-shrink-0",
+              isActive(link.href) && "text-sky-500"
+            )}
+          />
+          <span>{link.label}</span>
+        </Link>
+      ))}
+
+      <Heading as="p" className="text-sm md:text-sm lg:text-sm pt-10 px-2">
+        Socials
+      </Heading>
+      {socials.map((link: Navlink) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className={twMerge(
+            "text-secondary hover:text-primary transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm"
+          )}
+        >
+          <link.icon
+            className={twMerge(
+              "h-4 w-4 flex-shrink-0",
+              isActive(link.href) && "text-sky-500"
+            )}
+          />
+          <span>{link.label}</span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+const SidebarHeader = () => {
+  return (
+    <div className="flex flex-col items-center mb-6">
+      <motion.div
+        whileTap={{
+          scale: [1, 1.3, 1],
+          rotate: [0, 10, -10, 0],
+          y: [-5, 0],
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 300,
+          damping: 10,
+        }}
+      >
+        <Image
+          src="/images/profile-picture/profile.jpg"
+          alt="Yunus Emre Koyun"
+          width={80}
+          height={80}
+          className="rounded-full object-cover aspect-square"
+        />
+      </motion.div>
+
+      <p className="font-bold text-primary text-base mt-2">Yunus Emre Koyun</p>
+      <p className="font-light text-secondary text-sm">Fullstack Developer</p>
+    </div>
+  );
+};
